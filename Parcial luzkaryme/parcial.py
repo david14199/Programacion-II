cc78603cc09dab15c1145f50820213721884d51d
david14199 <72474047+david14199@users.noreply.github.com>
2024-04-23 19:40:13 -0500

    Add files via upload

---
+++ Parcial luzkaryme/parcial.py
@@ -0,0 +1,30 @@
+
+SJF=[]
+
+FIFO=[]
+rafa=[]
+hola=True
+while (hola):
+    print("1.FIFO")
+    print("2.SJF")
+    print("3.Mostrar cola de FIFO")
+    print("4.Mostrar cola de SJF")
+    print("5.cerrrar \n")
+    opcion=int(input("Ingrese la opcion "))
+    if(opcion==1):
+        fifo=input("Ingrese los procesos a añadir a algoritmo de fifo en su orden ")
+        FIFO.append(fifo)
+    if(opcion==2):
+        print("SJF")
+    
+    if(opcion==3):
+        for i in range (len(FIFO)):
+            print(i,FIFO[i])
+        print("")
+
+    if(opcion==4):
+        print("")
+    if(opcion==5):
+        print("adios")
+        hola=False
+
